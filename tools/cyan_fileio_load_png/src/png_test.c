13fdcdbedd8a3343494ab3a9e410e4d45a58f097
DavyL <cr4zypxl@gmail.com>
2020-04-14 14:45:02 +0200

    Included save_ppm() in cyan_fileio

--- tools/cyan_fileio_load_png/src/png_test.c
+++ tools/cyan_fileio_load_png/src/png_test.c
@@ -1,9 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <errno.h>
 
-int main(){
+#include <cyan/image/image.h>
 
-	fprintf(stdout, "Hello world.\n");
+#include <cyan_fileio/load_png.h>
+#include <cyan_fileio/save_ppm.h>
+
+int main(int argc, char** argv, char * envv){
+
+	if(argc != 2){
+		fprintf(stderr, "Usage : %s file.png\n", argv[0] );
+		return -1;
+	}
+	
+	FILE * fp = NULL;
+	fp = fopen(argv[1], "r");
+	if(fp == NULL){
+		fprintf(stderr, "Couldn't open file. \n Error : %d, (%s)\n", errno, strerror(errno));
+		return -1;
+	}
+
+	image_t * image = NULL;
+	image = png2image(fp);
+	if(image == NULL){
+		fprintf(stderr, "Image allocation failed\n");
+		return -1;
+	}
+
+	image_save_ppm( image, "image.ppm" );
+	image_free(image);
+	fclose(fp);
 
 	return 0;
 }
