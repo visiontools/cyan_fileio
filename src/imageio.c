b98a1beabac574afadbe14f29daa2ffd1b87d013
Sebastien Druon <seb@homedruon.com>
2020-05-22 18:07:41 +0200

    dirty but works

--- src/imageio.c
+++ src/imageio.c
@@ -18,6 +18,9 @@ int image_import( image_t** img, char* path ) {
     double* X ;
     double* Y ;
     double* Z ;
+    double R ;
+    double G ;
+    double B ;
 
     MagickCoreGenesis( (char*) NULL,MagickTrue);
     exception=AcquireExceptionInfo();
@@ -35,10 +38,14 @@ int image_import( image_t** img, char* path ) {
     if (*img == NULL ) {
         *img = image_new( image->rows, image->columns ) ;
     } else {
+        
+        // Verifier taille 
+        // Verifier que ce n'est pas une image mono
+        
+
         image_resize ( *img, image->rows, image->columns, NULL ) ;  
     }
 
-    TransformImageColorspace( image, XYZColorspace, exception ) ;
     buffer = GetAuthenticPixels( image, 0, 0, image->columns, image->rows, exception ) ;
     X = (*img)->X ;
     Y = (*img)->Y ;
@@ -46,14 +53,15 @@ int image_import( image_t** img, char* path ) {
 
     for (i=0; i<image->columns; i++ )
         for (j=0; j<image->rows; j++ ) {
-            *X = (double) *buffer / QuantumRange ;
+            R = (double) *buffer / QuantumRange ;
             buffer++ ;
+            G = (double) *buffer / QuantumRange ;
+            buffer++ ;
+            B = (double) *buffer / QuantumRange ;
+            buffer++ ;
+            RGB_to_XYZ( CYAN_RGB_sRGB, R,G,B, X, Y, Z ) ;
             X++ ;
-            *Y = (double) *buffer / QuantumRange ;
-            buffer++ ;
             Y++ ;
-            *Z = (double) *buffer / QuantumRange ;
-            buffer++ ;
             Z++ ;
         }
 
@@ -83,6 +91,9 @@ int image_export( image_t* img, const char* path ) {
     double* pt_x ;
     double* pt_y ;
     double* pt_z ;
+    double R ;
+    double G ;
+    double B ;
 
     pt_x = img->X ;
     pt_y = img->Y ;
@@ -91,9 +102,12 @@ int image_export( image_t* img, const char* path ) {
     for (j=0; j<img->rows; j++ ) {
         for (i=0; i<img->cols; i++ ) {
 
-        buffer[ 3*(i+j*img->cols) ]   = *pt_x ;
-        buffer[ 3*(i+j*img->cols)+1 ] = *pt_y ;
-        buffer[ 3*(i+j*img->cols)+2 ] = *pt_z;
+        XYZ_to_RGB( CYAN_RGB_sRGB, *pt_x, *pt_y, *pt_z , &R, &G, &B ) ;
+        
+
+        buffer[ 3*(i+j*img->cols) ]   = R ;
+        buffer[ 3*(i+j*img->cols)+1 ] = G ;
+        buffer[ 3*(i+j*img->cols)+2 ] = B ;
         
         pt_x++ ;
         pt_y++ ;
@@ -107,15 +121,17 @@ int image_export( image_t* img, const char* path ) {
     exception=AcquireExceptionInfo();
     exception2=AcquireExceptionInfo();
     image_info=CloneImageInfo((ImageInfo *) NULL);
-    strcpy(image->filename,path);
 
     image = ConstituteImage(img->cols,img->rows,"RGB",DoublePixel,buffer, exception);
     if (exception->severity != UndefinedException)
         CatchException(exception);
 
+    strcpy(image->filename,path);
+    
     image_info->file = fopen ("test.png", "w+b");
     strcpy (image_info->filename, "test.png");
     strcpy (image_info->magick, "png");
+   
     WriteImage( image_info, image, exception2 ) ; 
     fclose( image_info->file ) ;
     if (exception2->severity != UndefinedException)
@@ -123,7 +139,8 @@ int image_export( image_t* img, const char* path ) {
 
     MagickCoreTerminus();
 
-    free(image) ;
+    free(image) ;       // FIXME
+    // free( buffer ) ; ?
 
     return ERR_OK ;
 }
