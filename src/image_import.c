77ec796299538ca5ebc92663a338c215aaeb52fd
Sebastien Druon <seb@homedruon.com>
2020-05-24 22:27:55 +0200

    done with import

--- src/image_import.c
+++ src/image_import.c
@@ -7,6 +7,9 @@
 #include <cyan/image/image.h>
 #include <cyan/common/error.h>
 
+void sRGB_convert( Image* image, double**X, double**Y, double **Z, Quantum** Buffer ) ;
+void GRAY_convert( Image* image, double**X, double**Y, double **Z, Quantum** Buffer ) ;
+
 
 int image_import( image_t** img, char* path ) {
 
@@ -21,63 +24,94 @@ int image_import( image_t** img, char* path ) {
     double R ;
     double G ;
     double B ;
+    void (*fct_convert)(Image*,double**,double**,double**,Quantum**) ;
 
+    ColorspaceType colorspace ;
+    int monochrome = 0 ;
+    
     MagickCoreGenesis( (char*) NULL,MagickTrue);
+    
     exception=AcquireExceptionInfo();
+    
     image_info=CloneImageInfo((ImageInfo *) NULL);
     strcpy(image_info->filename,path);
+    
     image=ReadImage(image_info,exception);
-
     if (exception->severity != UndefinedException)
         CatchException(exception);
-    if (image == (Image *) NULL)
-        exit(1);
+    if (image == (Image *) NULL) {
+        CYAN_ERROR( ERR_MALLOC ) ;
+        return ERR_MALLOC ;
+    }
+
+    // --- 
+
+    colorspace = GetImageColorspaceType( image, exception) ;
+    if (exception->severity != UndefinedException)
+        CatchException(exception);
+
+    printf("Cols: %ld \n", image->columns ) ;
+    printf("Rows: %ld \n", image->rows ) ;
+    printf("Colorspace: %d \n", colorspace ) ;
+
+    switch( colorspace ) {
+        case sRGBColorspace:
+            monochrome=0 ;
+            fct_convert = sRGB_convert ;
+            break ;
+        case xyYColorspace:
+            CYAN_ERROR( ERR_NOT_IMPLEMENTED ) ;     // TODO
+            return ERR_NOT_IMPLEMENTED ;
+            break ;
+        case XYZColorspace: 
+            CYAN_ERROR( ERR_NOT_IMPLEMENTED ) ;     // TODO
+            return ERR_NOT_IMPLEMENTED ;
+            break ;
+        case LabColorspace:
+            CYAN_ERROR( ERR_NOT_IMPLEMENTED ) ;     // TODO
+            return ERR_NOT_IMPLEMENTED ;
+            break ;
+        case LCHabColorspace:   
+            CYAN_ERROR( ERR_NOT_IMPLEMENTED ) ;     // TODO
+            return ERR_NOT_IMPLEMENTED ;
+            break ;
+        case LuvColorspace:
+            CYAN_ERROR( ERR_NOT_IMPLEMENTED ) ;     // TODO
+            return ERR_NOT_IMPLEMENTED ;
+            break ;
+        case LCHuvColorspace:
+            CYAN_ERROR( ERR_NOT_IMPLEMENTED ) ;     // TODO
+            return ERR_NOT_IMPLEMENTED ;
+            break ;
+        case GRAYColorspace:
+            monochrome=1 ;
+            fct_convert = GRAY_convert ;
+            break ;
+        default:
+            CYAN_ERROR( ERR_NOT_IMPLEMENTED ) ; 
+            return ERR_NOT_IMPLEMENTED ;
+            break ;
+    }
 
     // ---
 
     if (*img == NULL ) {
-        *img = image_new( image->rows, image->columns ) ;
+        *img = image_new( image->columns, image->rows, monochrome ) ;
     } else {
-        
-        // Verifier taille 
-        // Verifier que ce n'est pas une image mono
-        
-
-        image_resize ( *img, image->rows, image->columns, NULL ) ;  
+       image_strip_data( *img ) ;
+       if (((*img)->rows!=image->rows)||((*img)->cols!=image->columns)||((*img)->monochrome!=monochrome))
+            image_resize ( *img, image->columns, image->rows, monochrome, NULL ) ;  
     }
 
     buffer = GetAuthenticPixels( image, 0, 0, image->columns, image->rows, exception ) ;
+
     X = (*img)->X ;
     Y = (*img)->Y ;
     Z = (*img)->Z ;
 
     for (i=0; i<image->columns; i++ )
         for (j=0; j<image->rows; j++ ) {
-            R = (double) *buffer / QuantumRange ;
-            buffer++ ;
-            G = (double) *buffer / QuantumRange ;
-            buffer++ ;
-            B = (double) *buffer / QuantumRange ;
-            buffer++ ;
-            
-            RGB_to_XYZ( CYAN_RGB_sRGB, R,G,B, X, Y, Z ) ;
-            
-            // if ((i==0) && (j==0 )) {
-            //     
-            //     printf("Read : R = %lf \n", R ) ;
-            //     printf("Read : G = %lf \n", G ) ;
-            //     printf("Read : B = %lf \n", B ) ;
-            //     printf("\n");
-            //     printf("Read : X = %lf \n", *X ) ;
-            //     printf("Read : Y = %lf \n", *Y ) ;
-            //     printf("Read : Z = %lf \n", *Z ) ;
-            //     printf("\n");
-            // 
-            // }
-            
-            X++ ;
-            Y++ ;
-            Z++ ;
+            fct_convert( image, &X,&Y,&Z,&buffer) ;
         }
 
     // ---
@@ -90,3 +124,24 @@ int image_import( image_t** img, char* path ) {
     return ERR_OK ;
 }
 
+void sRGB_convert( Image* image, double**X, double**Y, double **Z, Quantum **buffer) {
+            double R ;
+            double G ;
+            double B ;
+            R = (double) **buffer / QuantumRange ;
+            (*buffer)++ ;
+            G = (double) **buffer / QuantumRange ;
+            (*buffer)++ ;
+            B = (double) **buffer / QuantumRange ;
+            (*buffer)++ ;
+            RGB_to_XYZ( CYAN_RGB_sRGB, R,G,B, *X, *Y, *Z ) ;
+            (*X)++ ;
+            (*Y)++ ;
+            (*Z)++ ;
+}
+
+void GRAY_convert( Image* image, double**X, double**Y, double **Z, Quantum **buffer) {
+            **Y = (double) **buffer / QuantumRange ;
+            (*buffer)++ ;
+            (*Y)++ ;
+}
